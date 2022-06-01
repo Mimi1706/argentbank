d674e32bb13f66afc72c8248146e1d72a927ace0
Mimi <hana.nguyen@outlook.fr>
2022-06-01 16:23:43 +0200

    FeatureItem updated

---
+++ src/data/featureItems.js
@@ -0,0 +1,21 @@
+import featureItemImg1 from "../assets/FeatureItem/icon-chat.png"
+import featureItemImg2 from "../assets/FeatureItem/icon-money.png"
+import featureItemImg3 from "../assets/FeatureItem/icon-security.png"
+
+export const featureItems = [
+  {
+    img: featureItemImg1,
+    title: "You are our #1 priority",
+    text: "Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes.",
+  },
+  {
+    img: featureItemImg2,
+    title: "More savings means higher rates",
+    text: "The more you save with us, the higher your interest rate will be!",
+  },
+  {
+    img: featureItemImg3,
+    title: "Security you can trust",
+    text: "We use top of the line encryption to make sure your data and money is always safe. ",
+  },
+]
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,9 +3,7 @@ import "./Home.css"
 import heroBannerImg from "../../assets/banner.jpeg"
 import FeatureItem from "../../components/FeatureItem/FeatureItem"
 
-import featureItemImg1 from "../../assets/FeatureItem/icon-chat.png"
-import featureItemImg2 from "../../assets/FeatureItem/icon-money.png"
-import featureItemImg3 from "../../assets/FeatureItem/icon-security.png"
+import { featureItems } from "../../data/featureItems"
 
 const Home = () => {
   return (
@@ -24,21 +22,14 @@ const Home = () => {
         </div>
       </div>
       <section className="featureItems">
-        <FeatureItem
-          img={featureItemImg1}
-          title="You are our #1 priority"
-          text="Need to talk to a representative? You can get in touch through our 24/7 chat or through a phone call in less than 5 minutes."
-        />
-        <FeatureItem
-          img={featureItemImg2}
-          title="More savings means higher rates"
-          text="The more you save with us, the higher your interest rate will be!"
-        />
-        <FeatureItem
-          img={featureItemImg3}
-          title="Security you can trust"
-          text="We use top of the line encryption to make sure your data and money is always safe. "
-        />
+        {featureItems.map(({ img, title, text }, index) => (
+          <FeatureItem
+            key={`featureItem-${index}`}
+            title={title}
+            img={img}
+            text={text}
+          />
+        ))}
       </section>
     </main>
   )
